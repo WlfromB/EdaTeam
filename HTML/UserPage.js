e83547f21108e1c598074e5f1729c8b2c6fdd357
WlfromB <sasha567883@gmail.com>
2024-04-08 18:13:30 +0300

    добавил анимацию окошка входа. добавил фон для странички

--- HTML/UserPage.js
+++ HTML/UserPage.js
@@ -1,16 +1,25 @@
 const modal = document.getElementById("loginModal");
 const loginLink = document.getElementById("loginLink");
 const closeBtn = document.getElementsByClassName("close")[0];
+const loginFormContainer = document.getElementsByClassName("modal-content")[0];
 
 // Обработчик события для открытия модального окна при клике на ссылку
 loginLink.addEventListener("click", (event) => {
   event.preventDefault();
   modal.style.display = "block";
+  loginFormContainer.style.transform = 'translateY(-36rem)';
+  setTimeout(()=>{
+    loginFormContainer.style.transform = 'translateY(0rem)';
+  }, 10);
 });
 
 // Обработчик события для закрытия модального окна при клике на крестик
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+closeBtn.addEventListener("click", () => {  
+  loginFormContainer.style.transform = 'translateY(0rem)';
+  setTimeout(()=>{
+    loginFormContainer.style.transform = 'translateY(-36rem)';
+  }, 10);
+  setTimeout(()=>{modal.style.display = "none"},510);
 });
 
 // Обработчик события для закрытия модального окна при клике за его пределами
