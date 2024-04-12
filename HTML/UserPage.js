d3552e2b42579f23dced4a04b5b8f9ba82a29412
WlfromB <sasha567883@gmail.com>
2024-04-12 20:37:24 +0300

    добавил форму регистрации и ссылку в форме входа на нее

--- HTML/UserPage.js
+++ HTML/UserPage.js
@@ -2,6 +2,16 @@ const modal = document.getElementById("loginModal");
 const loginLink = document.getElementById("loginLink");
 const closeBtn = document.getElementsByClassName("close")[0];
 const loginFormContainer = document.getElementsByClassName("modal-content")[0];
+const siginLink = document.getElementById("signinLink");
+
+siginLink.addEventListener("click", (event)=>{
+  event.preventDefault();
+  loginFormContainer.style.transform = 'translateY(0rem)';
+    setTimeout(()=>{
+      loginFormContainer.style.transform = 'translateY(-36rem)';
+    }, 10);
+    setTimeout(()=>{modal.style.display = "none"},510);
+})
 
 // Обработчик события для открытия модального окна при клике на ссылку
 loginLink.addEventListener("click", (event) => {
@@ -32,3 +42,4 @@ window.addEventListener("click", (event) => {
     setTimeout(()=>{modal.style.display = "none"},510);
   }
 });
+
