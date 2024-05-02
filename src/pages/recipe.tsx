fb3f709ef3432e20395da85dbe04be6eafdbf466
Adelina20034 <sat_ade@mail.ru>
2024-05-02 20:39:41 +0300

    (без названия)

--- src/pages/recipe.tsx
+++ src/pages/recipe.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 
 import { Footer } from '../components/footer';
-import { Header } from '../components/Header';
+import { Header } from '../components/header';
 import {Main} from "../components/rp-main";
 
 const Recipe = () => {
