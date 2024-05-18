13933b139d42e2493aa9852e1673ab763c151ae5
WlfromB <sasha567883@gmail.com>
2024-05-19 01:43:40 +0300

    ссылки из футера ведут в новую вкладку

--- src/components/footer-link/footer-link.tsx
+++ src/components/footer-link/footer-link.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import { StyledFooterCaption } from "./footer-link.styled";
 import { StyledFooterTg } from "./footer-link.styled";
@@ -8,8 +8,14 @@ import { StyledFooterLink } from "./footer-link.styled";
 
 
 export const FooterLink = (props)=>{
+    const isExternal = useMemo(()=> props.href?.startsWith('http'), [props.href])
+    const linkProps: any = {}
+    if (isExternal) {
+        linkProps.target = '_blank'
+        linkProps.rel = 'noopener noreferrer'
+      }
     return (
-    <StyledFooterLink href={props.href} className="link-tg"> 
+    <StyledFooterLink href={props.href} className="link-tg" {...linkProps}> 
         <StyledFooterTg className="logo-tg" src={logo_tg} alt="Логотип телеграма"/> 
         <StyledFooterCaption className="link-tg-text">{props.name}</StyledFooterCaption> 
     </StyledFooterLink> 
