a76ab585598b3389a8b44d78235fb39a1ae7866b
WlfromB <sasha567883@gmail.com>
2024-05-19 14:21:23 +0300

    rename files

--- src/components/user-page-right-block/index.ts
+++ src/components/user-page-right-block/index.ts
@@ -1,4 +1,4 @@
-export {UserPageRightBlock} from './UserPage-RightBlock';
+export {UserPageRightBlock} from './user-page-right-block';
 
 export {default as userIcon} from '../../assets/UserPage/Icons/Аватарка профиля.svg';
 export {default as userHeart} from '../../assets/UserPage/Icons/heart-red-svgrepo-com.svg';
