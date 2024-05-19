fd1333b86b1048ac6a77a32d64a58bbc72252b0d
WlfromB <sasha567883@gmail.com>
2024-05-19 14:52:53 +0300

    ссылка на главную страницу в лого и linter

--- src/components/image-logo/image-logo.tsx
+++ src/components/image-logo/image-logo.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import { userpagelogo } from "./index";
 import { StyledImageLogo } from "./image-logo.styled";
+import { Link } from "../link";
+import { URLs } from "../../__data__/urls";
 
 export const ImageLogo = ()=>{
-    return <StyledImageLogo 
+    return (
+    <Link Href={`${URLs.baseUrl}`} ClassName=" ">
+    <StyledImageLogo 
     src={userpagelogo}        
     alt="Логотип фирмы: вилка и нож"
     />
+    </Link>
+    )
 }
